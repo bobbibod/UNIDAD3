fdefa1c28bc87615f553c8ae02149f3849f92b9b
bobbibod <156532586+bobbibod@users.noreply.github.com>
2025-10-21 03:42:05 +0000

    AVANCE SPOOL V1

--- UNIDAD3/SpoolV1.java
+++ UNIDAD3/SpoolV1.java
@@ -1,23 +1,35 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class SpoolV1 {
 	public static void main(String[] args) {
-		Spool<ArchivoV2> sp= new Spool(10);
-		int random=Rutinas.nextInt(1,10);
-		String [] Extensiones={"WORD","PDF","DOC",""};
-		for(int j=0 ; j<3 ; j++) {
+		int tamanoSpool=30;
+		Spool<ArchivoV2> sp= new Spool(tamanoSpool);
+		
+		String [] Extensiones={"WORD","PDF","DOC"};
+		int noDeArchivos=Rutinas.nextInt(10);
+	
+		for(int j=0 ; j<noDeArchivos ; j++) {
 			int totalPaginas=Rutinas.nextInt(1,500);
+			int random=Rutinas.nextInt(1,10);
+			int IndiceExt=Rutinas.nextInt(0,2);
+			System.out.println();
 			for(int i=0 ; i< totalPaginas/100;i++) {
-				
-				System.out.println(sp.Enviar(new ArchivoV2("D"+random,"WORD",10,totalPaginas,100) )  );
-
+				sp.Enviar(new ArchivoV2("D"+random,Extensiones[IndiceExt],10,totalPaginas,100) );
+					tamanoSpool--;
 			}
+		
 			if(totalPaginas%100 > 0 ) {
-				System.out.println(sp.Enviar(new ArchivoV2("D1","WORD",10,totalPaginas,totalPaginas%100) )  );
-
-			}	
-
+				sp.Enviar(new ArchivoV2("D"+random,Extensiones[IndiceExt],10,totalPaginas,totalPaginas%100) );
+					tamanoSpool--;
+			}
+			System.out.println("ESPACION RESTANTE EN EL SPOOL: "+tamanoSpool);
 		}
+		while(sp.Imprimir()){
+			System.out.println(sp.getDr());
+			tamanoSpool=tamanoSpool+1;}
+	
+	System.out.println(tamanoSpool);
 
 }
 
